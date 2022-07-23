719b30fc044ce7ce6df5381be75b7c5a05a9dab7
hyunbell <man.of.backend@gmail.com>
2022-07-24 05:04:14 +0900

    feat: main 진행중

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,18 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Feed from '../components/Feed';
 import Sidebar from '../components/Sidebar';
+import Widgets from '../components/Widgets';
 
 const Home: NextPage = () => {
 	return (
-		<div className="flex min-h-screen flex-col items-center justify-center py-2">
+		<div className="mx-auto max-h-screen overflow-hidden lg:max-w-6xl">
 			<Head>
 				<title>Twitter 2.0</title>
 			</Head>
-			<main>
+			<main className="grid grid-cols-9">
 				<Sidebar />
 				<Feed />
+				<Widgets />
 			</main>
 		</div>
 	);
