f1a45253681a6f409f8cc9401184db1963e22e52
hyunbell <man.of.backend@gmail.com>
2022-08-07 05:01:30 +0900

    fix: vercel_Url

--- utils/fetchComments.ts
+++ utils/fetchComments.ts
@@ -2,7 +2,7 @@ import { Comment } from '../typings';
 
 const host =
 	process.env.NODE_ENV === 'production'
-		? process.env.NEXT_PUBLIC_VERCEL_URL
+		? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
 		: process.env.NEXT_PUBLIC_BASE_URL;
 export const fetchComments = async (tweetId: string) => {
 	const res = await fetch(`${host}/api/getComments?tweetId=${tweetId}`);
