a871c391ad47c26ef9e43104ee9e61468f33916b
hyunbell <man.of.backend@gmail.com>
2022-08-07 04:50:46 +0900

    fix: host url

--- utils/fetchComments.ts
+++ utils/fetchComments.ts
@@ -1,9 +1,11 @@
 import { Comment } from '../typings';
 
+const host =
+	process.env.NODE_ENV === 'production'
+		? process.env.VERCEL_URL
+		: process.env.NEXT_PUBLIC_BASE_URL;
 export const fetchComments = async (tweetId: string) => {
-	const res = await fetch(
-		`${process.env.NEXT_PUBLIC_BASE_URL}/api/getComments?tweetId=${tweetId}`,
-	);
+	const res = await fetch(`${host}/api/getComments?tweetId=${tweetId}`);
 
 	const comments: Comment[] = await res.json();
 
--- utils/fetchTweets.ts
+++ utils/fetchTweets.ts
@@ -1,7 +1,11 @@
 import { Tweet } from '../typings';
 
+const host =
+	process.env.NODE_ENV === 'production'
+		? process.env.VERCEL_URL
+		: process.env.NEXT_PUBLIC_BASE_URL;
 export const fetchTweets = async () => {
-	const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getTweets`);
+	const res = await fetch(`${host}/api/getTweets`);
 
 	const data = await res.json();
 	const tweets: Tweet[] = data.tweets;
